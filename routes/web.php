847efea2007f2303bbf187339eb13d98086b0b05
Dhamar16 <marrrcahay@gmail.com>
2026-01-29 14:55:47 +0700

    fix route

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,23 @@
 <?php
 
-use App\Http\Controllers\admin\KategoriController;
-use App\Http\Controllers\Admin\UserController;
-use App\Http\Controllers\admin\AlatController;
-use App\Http\Controllers\Admin\DashboardController;
-use App\Http\Controllers\admin\PeminjamanController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\Peminjaman;
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\Admin\KategoriController;
+use App\Http\Controllers\Admin\AlatController;
+use App\Http\Controllers\Admin\PeminjamanController;
 
+/*
+|--------------------------------------------------------------------------
+| Redirect awal
+|--------------------------------------------------------------------------
+*/
 Route::get('/', function () {
     return redirect()->route('login');
-}); 
-// Auth Routes
+});
+
+    // Auth Route
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 
@@ -21,24 +26,39 @@ Route::post('/register', [AuthController::class, 'register']);
 
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-
-// Admin Routes
-Route::get('/admin/dashboard', [DashboardController::class, 'index'])
+// admin route
+Route::prefix('admin')
     ->middleware(['auth', 'admin'])
-    ->name('admin.dashboard');
+    ->name('admin.')
+    ->group(function () {
 
-// Petugas Routes
-Route::get('/petugas/dashboard', function () {
-    return view('petugas.dashboard');
-})->middleware(['auth','petugas']);
+        Route::get('/dashboard', [DashboardController::class, 'index'])
+            ->name('dashboard');
 
-// Peminjam Routes
-Route::get('/peminjam/dashboard', function () {
-    return view('peminjam.dashboard');
-})->middleware(['auth','peminjam']);
+        Route::resource('/users', UserController::class);
+        Route::resource('/kategori', KategoriController::class);
+        Route::resource('/alat', AlatController::class);
+        Route::resource('/peminjaman', PeminjamanController::class);
+    });
 
+    // petugas route
+Route::prefix('petugas')
+    ->middleware(['auth', 'petugas'])
+    ->name('petugas.')
+    ->group(function () {
 
-Route::resource('/admin/users', UserController::class);
-Route::resource('/admin/kategori', KategoriController::class);
-Route::resource('/admin/alat', AlatController::class);
-Route::resource('/admin/peminjaman', PeminjamanController::class);
+        Route::get('/dashboard', function () {
+            return view('petugas.dashboard');
+        })->name('dashboard');
+    });
+
+// peminjam route
+Route::prefix('peminjam')
+    ->middleware(['auth', 'peminjam'])
+    ->name('peminjam.')
+    ->group(function () {
+
+        Route::get('/dashboard', function () {
+            return view('peminjam.dashboard');
+        })->name('dashboard');
+    });
